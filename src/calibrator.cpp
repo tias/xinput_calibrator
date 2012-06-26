07978b90b81237d83071426643416161087e45dd
Tias Guns <tias@ulyssis.org>
2012-06-26 21:20:59 +0200

    Rewrite and simplify swapping
    Inspired by forest bond's 'calibrator: Convert to device coordinates
    immediately.' patch:
    https://github.com/forest-bond/xinput_calibrator/commit/12832527fba6dd1b3135751257ca6a734aa571cd

--- src/calibrator.cpp
+++ src/calibrator.cpp
@@ -142,23 +142,19 @@ bool Calibrator::finish(int width, int height)
     // based on old_axys: inversion/swapping is relative to the old axis
     XYinfo new_axis(old_axys);
 
-    // Should x and y be swapped?
-    bool do_swap_xy = false;
-    if (abs(clicked.x[UL] - clicked.x[UR]) < abs(clicked.y[UL] - clicked.y[UR])) {
-        do_swap_xy = true;
-        new_axis.do_swap_xy();
-    }
-
-    if (do_swap_xy) {
-        std::swap(clicked.x[LL], clicked.x[UR]);
-        std::swap(clicked.y[LL], clicked.y[UR]);
-    }
 
     // calculate average of clicks
     float x_min = (clicked.x[UL] + clicked.x[LL])/2.0;
     float x_max = (clicked.x[UR] + clicked.x[LR])/2.0;
     float y_min = (clicked.y[UL] + clicked.y[UR])/2.0;
     float y_max = (clicked.y[LL] + clicked.y[LR])/2.0;
+
+    // Should x and y be swapped?
+    if (abs(clicked.x[UL] - clicked.x[UR]) < abs(clicked.y[UL] - clicked.y[UR])) {
+        new_axis.swap_xy = !new_axis.swap_xy;
+        std::swap(x_min, y_min);
+        std::swap(x_max, y_max);
+    }
 
     // the screen was divided in num_blocks blocks, and the touch points were at
     // one block away from the true edges of the screen.
@@ -184,12 +180,7 @@ bool Calibrator::finish(int width, int height)
     y_max = scaleAxis(y_max, old_axys.y.max, old_axys.y.min, height, 0);
 
 
-    // If x and y has to be swapped we also have to swap the parameters
-    if (do_swap_xy) {
-        std::swap(x_min, y_max);
-        std::swap(y_min, x_max);
-    }
-
+    // round and put in new_axis struct
     new_axis.x.min = round(x_min); new_axis.x.max = round(x_max);
     new_axis.y.min = round(y_min); new_axis.y.max = round(y_max);
 
