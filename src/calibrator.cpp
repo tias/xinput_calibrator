tias/xinput_calibrator

commit bfeaf2eaa9e1792bd6169ba723aa0d285a755d16

two minor API cleanups

src/calibrator.hh
@@ -92,10 +92,6 @@ struct XYinfo {
     XYinfo(const XYinfo& old) :
         swap_xy(old.swap_xy), x(old.x), y(old.y) {}
 
-    void do_swap_xy() {
-        swap_xy = !swap_xy;
-    }
-
     void do_xf86ScaleAxis(const XYinfo& to, const XYinfo& from) {
         x.min = xf86ScaleAxis(x.min, to.x.max, to.x.min, from.x.max, from.x.min);
         x.max = xf86ScaleAxis(x.max, to.x.max, to.x.min, from.x.max, from.x.min);
@@ -186,9 +182,6 @@ protected:
 
     /// Apply new calibration, implementation dependent
     virtual bool finish_data(const XYinfo new_axys) =0;
-
-    /// Compute calibration on 1 axis
-    void process_axys( int screen_dim, const AxisInfo &previous, std::vector<int> &clicked, AxisInfo &updated );
 
     /// Check whether the given name is a sysfs device name
     bool is_sysfs_name(const char* name);

commit 2bc5f58237a565bd2b00b1a5a519b9d6249fcf18

Fix issue where calibration values for rotated screen were wrong
I think this commit fixes the issue reported by Tokoyami.  Calibration
values were really wrong.  The issue seemed to be caused by the commits
bfeaf2eaa (which is a broken commit) and 07978b90b8.  git-bisect told me
this.

Also removed duplication of calibration code "finish()" between evdev.cpp
and calibrator.cpp.  Added "compensateForDevice()" virtual function to
handle adjustments needed to compensate for the device.

src/calibrator.cpp
@@ -144,6 +144,12 @@ bool Calibrator::finish(int width, int height)
     // based on old_axys: inversion/swapping is relative to the old axis
     XYinfo new_axis(old_axys);
 
+    // Should x and y be swapped?
+    if (abs(clicked.x[UL] - clicked.x[UR]) < abs(clicked.y[UL] - clicked.y[UR])) {
+        new_axis.swap_xy = !new_axis.swap_xy;
+        std::swap(clicked.x[LL], clicked.x[UR]);
+        std::swap(clicked.y[LL], clicked.y[UR]);
+    }
 
     // calculate average of clicks
     float x_min = (clicked.x[UL] + clicked.x[LL])/2.0;
@@ -151,12 +157,11 @@ bool Calibrator::finish(int width, int height)
     float y_min = (clicked.y[UL] + clicked.y[UR])/2.0;
     float y_max = (clicked.y[LL] + clicked.y[LR])/2.0;
 
-    // Should x and y be swapped?
-    if (abs(clicked.x[UL] - clicked.x[UR]) < abs(clicked.y[UL] - clicked.y[UR])) {
-        new_axis.swap_xy = !new_axis.swap_xy;
-        std::swap(x_min, y_min);
-        std::swap(x_max, y_max);
-    }
+    // This has an empty implementation here, but derived classes
+    // may implement it to modify {x,y}_* and new_axis as needed.  
+    // See Evdev.cpp which uses this routine to compensate for 
+    // calculations done within the evdev device.
+    compensateForDevice( width, height, x_min, y_min, x_max, y_max, new_axis );
 
     // the screen was divided in num_blocks blocks, and the touch points were at
     // one block away from the true edges of the screen.
@@ -180,7 +185,6 @@ bool Calibrator::finish(int width, int height)
     x_max = scaleAxis(x_max, old_axys.x.max, old_axys.x.min, width, 0);
     y_min = scaleAxis(y_min, old_axys.y.max, old_axys.y.min, height, 0);
     y_max = scaleAxis(y_max, old_axys.y.max, old_axys.y.min, height, 0);
-
 
     // round and put in new_axis struct
     new_axis.x.min = round(x_min); new_axis.x.max = round(x_max);

src/calibrator.hh
@@ -171,8 +171,46 @@ public:
 
     /// add a click with the given coordinates
     bool add_click(int x, int y);
+
+    // Called by base class finish() method.
+    // Allows for derived class specific "adjustments" to be made
+    // just prior to the scaling and calibration calculations being
+    // performed.
+    //
+    // @param width
+    // The screen width, in pixels, as passed to calibrator::finish()
+    //
+    // @param height
+    // The screen height, in pixels, as passed to calibrator::finish()
+    //
+    // @param x_min
+    // The mean minimum x coordinate.  Calculated in Calibrator::finish()
+    // like so: float x_min = (clicked.x[UL] + clicked.x[LL])/2.0;
+    //
+    // @param y_min
+    // The mean minimum y coordinate.  Calculated in Calibrator::finish()
+    // like so: float y_min = (clicked.y[UL] + clicked.y[UR])/2.0;
+    //
+    // @param x_max
+    // The mean maximum x coordinate.  Calculated in Calibrator::finish()
+    // like so: float x_max = (clicked.x[UR] + clicked.x[LR])/2.0;
+    //
+    // @param y_max
+    // The mean maximum x coordinate.  Calculated in Calibrator::finish()
+    // like so: float y_max = (clicked.y[LL] + clicked.y[LR])/2.0;
+    //
+    // @param new_axis
+    // The axes calibration data, of type XYinfo, being set by calibrator:finish().
+    // This is ultimately passed as an arg to finish_data().
+    virtual void compensateForDevice( int width, int height, float &x_min, float &y_min,
+                                      float &x_max, float &y_max, XYinfo &new_axis ) {}
+
     /// calculate and apply the calibration
-    virtual bool finish(int width, int height);
+    bool finish(int width, int height);
+
+    
+
+
     /// get the sysfs name of the device,
     /// returns NULL if it can not be found
     const char* get_sysfs_name();

src/calibrator/Evdev.cpp
@@ -175,25 +175,44 @@ CalibratorEvdev::~CalibratorEvdev () {
     XCloseDisplay(display);
 }
 
-// From Calibrator but with evdev specific invertion option
-// KEEP IN SYNC with Calibrator::finish() !!
-bool CalibratorEvdev::finish(int width, int height)
+
+// Called by base class finish() method.
+// Allows for derived class specific "adjustments" to be made
+// just prior to the scaling and calibration calculations being
+// performed.
+//
+// @param width
+// The screen width, in pixels, as passed to calibrator::finish()
+//
+// @param height
+// The screen height, in pixels, as passed to calibrator::finish()
+//
+// @param x_min
+// The mean minimum x coordinate.  Calculated in Calibrator::finish()
+// like so: float x_min = (clicked.x[UL] + clicked.x[LL])/2.0;
+//
+// @param y_min
+// The mean minimum y coordinate.  Calculated in Calibrator::finish()
+// like so: float y_min = (clicked.y[UL] + clicked.y[UR])/2.0;
+//
+// @param x_max
+// The mean maximum x coordinate.  Calculated in Calibrator::finish()
+// like so: float x_max = (clicked.x[UR] + clicked.x[LR])/2.0;
+//
+// @param y_max
+// The mean maximum x coordinate.  Calculated in Calibrator::finish()
+// like so: float y_max = (clicked.y[LL] + clicked.y[LR])/2.0;
+//
+// @param new_axis
+// The axes calibration data, of type XYinfo, being set by calibrator:finish().
+// This is ultimately passed as an arg to finish_data().
+void CalibratorEvdev::compensateForDevice( int width, int height, float &x_min,
+                                           float &y_min, float &x_max,
+                                           float &y_max, XYinfo &new_axis )
 {
-    if (get_numclicks() != NUM_POINTS) {
-        return false;
+    if ( verbose ) {
+        printf( "DEBUG: compensating for evdev \"crazy\" code." );
     }
-
-    // new axis origin and scaling
-    // based on old_axys: inversion/swapping is relative to the old axis
-    XYinfo new_axis(old_axys);
-
-
-    // calculate average of clicks
-    float x_min = (clicked.x[UL] + clicked.x[LL])/2.0;
-    float x_max = (clicked.x[UR] + clicked.x[LR])/2.0;
-    float y_min = (clicked.y[UL] + clicked.y[UR])/2.0;
-    float y_max = (clicked.y[LL] + clicked.y[LR])/2.0;
-
 
     // When evdev detects an invert_X/Y option,
     // it performs the following *crazy* code just before returning
@@ -202,57 +221,20 @@ bool CalibratorEvdev::finish(int width, int height)
     if (old_axys.x.invert) {
         x_min = width - x_min;
         x_max = width - x_max;
+
         // avoid invert_x property from here on,
         // the calibration code can handle this dynamically!
         new_axis.x.invert = false;
     }
+
     if (old_axys.y.invert) {
         y_min = height - y_min;
         y_max = height - y_max;
+
         // avoid invert_y property from here on,
         // the calibration code can handle this dynamically!
         new_axis.y.invert = false;
     }
-    // end of evdev inversion crazyness
-
-
-    // Should x and y be swapped?
-    if (abs(clicked.x[UL] - clicked.x[UR]) < abs(clicked.y[UL] - clicked.y[UR])) {
-        new_axis.swap_xy = !new_axis.swap_xy;
-        std::swap(x_min, y_min);
-        std::swap(x_max, y_max);
-    }
-
-    // the screen was divided in num_blocks blocks, and the touch points were at
-    // one block away from the true edges of the screen.
-    const float block_x = width/(float)num_blocks;
-    const float block_y = height/(float)num_blocks;
-    // rescale these blocks from the range of the drawn touchpoints to the range of the 
-    // actually clicked coordinates, and substract/add from the clicked coordinates
-    // to obtain the coordinates corresponding to the edges of the screen.
-    float scale_x = (x_max - x_min)/(width - 2*block_x);
-    x_min -= block_x * scale_x;
-    x_max += block_x * scale_x;
-    float scale_y = (y_max - y_min)/(height - 2*block_y);
-    y_min -= block_y * scale_y;
-    y_max += block_y * scale_y;
-    
-    // now, undo the transformations done by the X server, to obtain the true 'raw' value in X.
-    // The raw value was scaled from old_axis to the device min/max, and from the device min/max
-    // to the screen min/max
-    // hence, the reverse transformation is from screen to old_axis
-    x_min = scaleAxis(x_min, old_axys.x.max, old_axys.x.min, width, 0);
-    x_max = scaleAxis(x_max, old_axys.x.max, old_axys.x.min, width, 0);
-    y_min = scaleAxis(y_min, old_axys.y.max, old_axys.y.min, height, 0);
-    y_max = scaleAxis(y_max, old_axys.y.max, old_axys.y.min, height, 0);
-
-
-    // round and put in new_axis struct
-    new_axis.x.min = round(x_min); new_axis.x.max = round(x_max);
-    new_axis.y.min = round(y_min); new_axis.y.max = round(y_max);
-
-    // finish the data, driver/calibrator specific
-    return finish_data(new_axis);
 }
 
 // Activate calibrated data and output it

src/calibrator/Evdev.hpp
@@ -60,8 +60,40 @@ public:
                     const bool use_timeout=false);
     ~CalibratorEvdev();
 
+    // Called by base class finish() method.
+    // Allows for derived class specific "adjustments" to be made
+    // just prior to the scaling and calibration calculations being
+    // performed.
+    //
+    // @param width
+    // The screen width, in pixels, as passed to calibrator::finish()
+    //
+    // @param height
+    // The screen height, in pixels, as passed to calibrator::finish()
+    //
+    // @param x_min
+    // The mean minimum x coordinate.  Calculated in Calibrator::finish()
+    // like so: float x_min = (clicked.x[UL] + clicked.x[LL])/2.0;
+    //
+    // @param y_min
+    // The mean minimum y coordinate.  Calculated in Calibrator::finish()
+    // like so: float y_min = (clicked.y[UL] + clicked.y[UR])/2.0;
+    //
+    // @param x_max
+    // The mean maximum x coordinate.  Calculated in Calibrator::finish()
+    // like so: float x_max = (clicked.x[UR] + clicked.x[LR])/2.0;
+    //
+    // @param y_max
+    // The mean maximum x coordinate.  Calculated in Calibrator::finish()
+    // like so: float y_max = (clicked.y[LL] + clicked.y[LR])/2.0;
+    //
+    // @param new_axis
+    // The axes calibration data, of type XYinfo, being set by calibrator:finish().
+    // This is ultimately passed as an arg to finish_data().
+    virtual void compensateForDevice( int width, int height, float &x_min, float &y_min,
+                                      float &x_max, float &y_max, XYinfo &new_axis );
+ 
     /// calculate and apply the calibration
-    virtual bool finish(int width, int height);
     virtual bool finish_data(const XYinfo new_axys);
 
     bool set_swapxy(const int swap_xy);